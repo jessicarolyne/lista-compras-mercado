a0b5d8a995871f53dd53974fb43e9e95c6c24974
Jessica Caroline <jessicarolyne@hotmail.com>
2023-10-26 22:42:03 -0300

    Atualização

--- appList/App.tsx
+++ appList/App.tsx
@@ -1,15 +1,25 @@
-import React from 'react';
-import Navigation from '@/navigation';
-import theme from '@/utils/theme';
-import { NavigationContainer } from '@react-navigation/native';
-import { ThemeProvider } from '@shopify/restyle';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
+import { useFonts, Inter_200ExtraLight,Inter_300Light, Inter_400Regular, Inter_700Bold, Inter_900Black } from '@expo-google-fonts/inter';
+import { theme } from './src/theme';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_200ExtraLight,Inter_300Light, Inter_400Regular, Inter_700Bold, Inter_900Black
+  })
   return (
-    <NavigationContainer>
-      <ThemeProvider theme={theme}>
-      <Navigation />
-      </ThemeProvider>
-    </NavigationContainer>
+    <View style={styles.container}>
+     {!fontsLoaded ? <Text>Carregou</Text> : <ActivityIndicator size={50} />}
+      <StatusBar style="auto" />
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: theme.colors.base.gray600,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
